5e86b329b53dcbe1c88ae8bdeb834c15c9656bd7
Zual Rana <freelancerzual@gmail.com>
2023-04-05 12:23:37 +0600

    validate file check

--- fileCheck.js
+++ fileCheck.js
@@ -1,5 +1,8 @@
 // check the input file is javascript file.
 function isJavascriptFile(fileName) {
+  if (typeof fileName !== "string") {
+    return "please provide a valid file name";
+  }
   const checkFile = fileName.endsWith(".js");
   if (checkFile) {
     return true;
