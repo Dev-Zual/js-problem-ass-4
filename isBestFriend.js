2759c95d786594291a06ea64cdb636ddd1f6d88c
Zual Rana <freelancerzual@gmail.com>
2023-04-05 12:37:43 +0600

    validate friend input value

--- isBestFriend.js
+++ isBestFriend.js
@@ -1,5 +1,8 @@
 // check if they were best friend
 function isBestFriend(friend1, friend2) {
+  if (typeof friend1 !== "object" || typeof friend2 !== "object") {
+    return "please provide a object";
+  }
   if (friend1.name == friend2.friend && friend1.friend == friend2.name) {
     return true;
   } else {
