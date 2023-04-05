4e88984ff22464c3b5bf1112fd9fc357543d0c0a
Zual Rana <freelancerzual@gmail.com>
2023-04-05 12:31:58 +0600

    validate oil input qty

--- oilPrice.js
+++ oilPrice.js
@@ -1,6 +1,14 @@
 // calculate all the oil price that you buy
 
 function oilPrice(dieselLtr, petrolLtr, octaneLtr) {
+  if (
+    typeof dieselLtr !== "number" ||
+    typeof petrolLtr !== "number" ||
+    typeof octaneLtr !== "number"
+  ) {
+    return "please provide a valid quantity";
+  }
+
   const dieselPerLtr = 114;
   const petrolPerLtr = 130;
   const octanePerLtr = 135;
