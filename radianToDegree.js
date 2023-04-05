9bd0600ae70db067c622357a3aa56ec84ce1dc97
Zual Rana <freelancerzual@gmail.com>
2023-04-05 12:18:38 +0600

    validate radian input

--- radianToDegree.js
+++ radianToDegree.js
@@ -1,7 +1,10 @@
 function radianToDegree(radian) {
+  if (typeof radian !== "number") {
+    return "please provide a valid input";
+  }
   const pi = Math.PI;
   const degree = (radian * 180) / pi;
   return degree.toFixed(2);
 }
 
-console.log(radianToDegree(199));
+console.log(radianToDegree("199"));
